e12d07403911aee31f35041b2e3d1914f268e235
SeanAndrie <gseanandrie@gmail.com>
2025-09-11 18:26:33 +0400

    chore: accounts for 0 source length

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*   By: sgadinga <sgadinga@student.42.abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 14:03:25 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/06/06 14:26:35 by sgadinga         ###   ########.fr       */
+/*   Updated: 2025/09/11 14:35:58 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	dst_len;
 
 	i = 0;
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
 	src_len = ft_strlen((char *)src);
 	dst_len = ft_strlen(dst);
 	if (size <= dst_len)
