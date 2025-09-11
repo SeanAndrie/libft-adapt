2af074f609cc387f758fc6d3921e0a3ffb24b240
SeanAndrie <gseanandrie@gmail.com>
2025-09-11 18:10:58 +0400

    fix: now uses memcpy for efficiency

--- src/ft_strjoin.c
+++ src/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sgadinga <sgadinga@student.42abudhabi.ae>  +#+  +:+       +#+        */
+/*   By: sgadinga <sgadinga@student.42.abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 22:31:00 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/06/06 22:40:57 by sgadinga         ###   ########.fr       */
+/*   Updated: 2025/09/11 14:33:28 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,19 @@
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*res;
-	size_t	s1_len;
-	size_t	total_len;
+	size_t	len1;
+	size_t	len2;
 
 	if (!s1 || !s2)
 		return (NULL);
-	s1_len = ft_strlen(s1);
-	total_len = s1_len + ft_strlen(s2);
-	res = malloc(sizeof(char) * (total_len + 1));
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	res = malloc(len1 + len2 + 1);
 	if (!res)
 		return (NULL);
-	ft_strlcat(res, s1, total_len + 1);
-	ft_strlcat(res + s1_len, s2, total_len + 1);
+	ft_memcpy(res, s1, len1);
+	ft_memcpy(res + len1, s2, len2);
+	res[len1 + len2] = '\0';
 	return (res);
 }
+
