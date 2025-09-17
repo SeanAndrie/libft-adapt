bc01187851560f5ad427af8274b15e7d5e8ced54
SeanAndrie <gseanandrie@gmail.com>
2025-09-17 12:56:37 +0400

    fix: made ft_vstrjoin null-safe

--- src/ft_vstrjoin.c
+++ src/ft_vstrjoin.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42.abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 21:49:51 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/08/18 22:54:53 by sgadinga         ###   ########.fr       */
+/*   Updated: 2025/09/17 12:54:41 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,16 @@ static size_t	get_total_len(unsigned int n, char *sep, va_list args)
 	size_t	i;
 	size_t	sep_len;
 	size_t	total_len;
+	char	*str;
 
 	i = 0;
 	total_len = 0;
 	sep_len = ft_strlen(sep);
 	while (i < n)
 	{
-		total_len += ft_strlen(va_arg(args, char *));
+		str = va_arg(args, char *);
+		if (str)
+			total_len += ft_strlen(str);
 		i++;
 	}
 	return (total_len + (sep_len * (n - 1)));
@@ -31,27 +34,27 @@ static size_t	get_total_len(unsigned int n, char *sep, va_list args)
 
 char	*ft_vstrjoin(unsigned int n, char *sep, ...)
 {
-	size_t	i;
 	size_t	len;
 	va_list	args;
+	char	*str;
 	char	*res;
 
 	if (n == 0)
-		return (NULL);
+		return (ft_strdup(""));
 	va_start(args, sep);
 	len = get_total_len(n, sep, args);
 	va_end(args);
-	res = ft_calloc(len + 1, sizeof(char *));
+	res = ft_calloc(len + 1, sizeof(char));
 	if (!res)
 		return (NULL);
 	va_start(args, sep);
-	i = 0;
-	while (i < n)
+	while (n-- > 0)
 	{
-		ft_strlcat(res, va_arg(args, char *), len + 1);
-		if (sep && i != n)
+		str = va_arg(args, char *);
+		if (str)
+			ft_strlcat(res, str, len + 1);
+		if (sep && n > 0)
 			ft_strlcat(res, sep, len + 1);
-		i++;
 	}
 	va_end(args);
 	return (res);
