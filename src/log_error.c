b9d83093a369148844431c815b2e3591c02bafc4
SeanAndrie <gseanandrie@gmail.com>
2025-10-13 01:41:35 +0400

    chore: added ERROR_NONE

--- src/log_error.c
+++ src/log_error.c
@@ -6,7 +6,7 @@
 /*   By: sgadinga <sgadinga@student.42.abudhabi.ae> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 14:45:21 by sgadinga          #+#    #+#             */
-/*   Updated: 2025/10/01 00:45:51 by sgadinga         ###   ########.fr       */
+/*   Updated: 2025/10/12 17:10:02 by sgadinga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 void	log_error(t_error_type type, const char *base, const char *format, ...)
 {
-	va_list	args;
+    va_list	args;
 
-	va_start(args, format);
-	if (base)
-		ft_dprintf(STDERR_FILENO, "%s: ", base);
-	if (type == ERROR_SYNTAX)
-		ft_dprintf(STDERR_FILENO, "syntax error: ");
-	else if (type == ERROR_RUNTIME)
-		ft_dprintf(STDERR_FILENO, "runtime error: ");
-	else
-		ft_dprintf(STDERR_FILENO, "warning: ");
-	ft_vdprintf(STDERR_FILENO, format, args);
-	va_end(args);
+    va_start(args, format);
+    if (base)
+        ft_dprintf(STDERR_FILENO, "%s: ", base);
+    if (type != ERROR_NONE)
+    {
+        if (type == ERROR_SYNTAX)
+            ft_dprintf(STDERR_FILENO, "syntax error: ");
+        else if (type == ERROR_RUNTIME)
+            ft_dprintf(STDERR_FILENO, "runtime error: ");
+        else if (type == ERROR_WARNING)
+            ft_dprintf(STDERR_FILENO, "warning: ");
+    }
+    ft_vdprintf(STDERR_FILENO, format, args);
+    va_end(args);
 }
